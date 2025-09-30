d60a809a24ef4f3cd35f626899a74af9f5b33664
virzarasydann <virzarasydan@gmail.com>
2025-09-30 17:01:39 +0700

    Update database.py

--- src/configuration/database.py
+++ src/configuration/database.py
@@ -5,7 +5,7 @@ from src.configuration.config import get_config
 config = get_config()
 
 DATABASE_URL = f"mysql+mysqlconnector://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_HOST}/{config.DB_NAME}"
-
+print(DATABASE_URL)
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
